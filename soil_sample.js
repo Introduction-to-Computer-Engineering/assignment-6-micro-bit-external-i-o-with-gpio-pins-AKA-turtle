18639b8b3fcad1b13579a1015afd985b32eaa912
AKA-turtle <54418597+AKA-turtle@users.noreply.github.com>
2019-10-31 14:10:56 -0600

    Update soil_sample.js

--- soil_sample.js
+++ soil_sample.js
@@ -2,7 +2,7 @@
 // Intro to CPE
 //Soil moisture sensor for Microbit
 
-let moistureReading = 0 //initializing variable
+let moistureReading:number = 0 //initializing variable
 basic.forever(function () { //our sampling loop
  pins.digitalWritePin(DigitalPin.P6, 1) //turns power for sensor on
  moistureReading = Math.map(pins.analogReadPin(AnalogPin.P0), 10, 750, 0, 4) //reads data from sensor and maps value ranges to 0 - 4
